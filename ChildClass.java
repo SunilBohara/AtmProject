77131859c9d7364424b45bfa528e7ce9439191bf
SunilBohara <61348668+SunilBohara@users.noreply.github.com>
2020-04-11 14:16:54 +0545

    Update ChildClass.java

--- ChildClass.java
+++ ChildClass.java
@@ -10,21 +10,31 @@ public class ChildClass extends Operations {
     public void checkingAccount(int mainchoice)
     {
         System.out.println("Type 1 - View Balance");
-        System.out.println("Type 2 - Withdraw Funds");
         System.out.println("Type 3 - Deposite Funds");
+        System.out.println("Type 2 - Withdraw Funds");
         System.out.println("Type 4 - Exit");
         System.out.print("Choice: ");
     }
-    
-    public void innerChoice(int innerChoice)
+
+    public int innerChoice(int innerChoice)
     {
+        Operations o = new Operations();
         switch(innerChoice)
         {
             case 1 :
-                Operations o = new Operations();
-                o.viewBalance(viewbalance);
+                o.viewBalance();
+                break;
+            case 2 :
+                o.depositeFunds();
+                break;
+            case 3 :
+                o.withdrawFunds();
+                break;
+            default :
+            System.exit(0);
+                break;
         }
+        return 0;
     }
 
 }
-
